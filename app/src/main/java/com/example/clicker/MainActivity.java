608fcb035421ccaf20a45846663b84d58eff41a8
Даниил <k1nd.ww@yandex.ru>
2025-02-15 11:14:49 +0300

    Redefined methods and redesigned

--- app/src/main/java/com/example/clicker/MainActivity.java
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.clicker;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private Integer count = 0;
+    private android.util.Log Log;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +26,60 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
-    public void onClickBatton(View view) {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("onStart", "onStart");
+        Toast.makeText(this, "onStart",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("onResume", "onResume");
+        Toast.makeText(this, "onResume",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("onPause", "onPause");
+        Toast.makeText(this, "onPause",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle autState){
+        super.onSaveInstanceState(autState);
+        autState.putInt("Тык", count);
+        Log.d("Instens","onSave");
+        Toast.makeText(this, "onSave",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle SavedState){
+        super.onRestoreInstanceState(SavedState);
+        if(SavedState != null && SavedState.containsKey("Тык")){
+            count = SavedState.getInt("Тык");
+        }
+        TextView txt = findViewById(R.id.txtCounter);
+        txt.setText(count.toString());
+        Log.d("Instens","get");
+        Toast.makeText(this, "get",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("onDestroy", "onDestroy");
+        Toast.makeText(this, "onDestroy!",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("onStop", "onStop");
+        Toast.makeText(this, "onStop!",Toast.LENGTH_LONG).show();
+    }
+    public void onClickButton(View view) {
         count++;
         TextView txt= findViewById(R.id.txtCounter);
         txt.setText(count.toString());
     }
-    public void onClickBatton2(View view) {
+    public void onClickButton2(View view) {
         count--;
         TextView txt= findViewById(R.id.txtCounter);
         txt.setText(count.toString());
